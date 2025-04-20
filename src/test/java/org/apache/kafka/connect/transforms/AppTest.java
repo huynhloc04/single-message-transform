cb84e9c722c2ae18d482aa07343609e5c8df84aa
edwinhuynh-novobi <edwin.huynh@novobi.com>
2025-04-20 20:55:25 +0700

    [INIT] first commit

---
+++ src/test/java/org/apache/kafka/connect/transforms/AppTest.java
@@ -0,0 +1,20 @@
+package org.apache.kafka.connect.transforms;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
